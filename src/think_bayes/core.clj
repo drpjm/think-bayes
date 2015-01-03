573d916ec77e5d94e110f81e289f09c5578c6463
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-03 13:26:32 -0500

    Started fleshing out the code for chapter 2 of think-bayes.

--- src/think_bayes/core.clj
+++ src/think_bayes/core.clj
@@ -1,6 +1,20 @@
 (ns think-bayes.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn distribution []
+  {})
+
+(defn set [dist item pr]
+  (assoc dist item pr))
+
+(defn prob [dist item]
+  (item dist))
+
+(defn increment [dist item]
+  (if (nil? (item dist))
+    (conj dist {item 0})
+    (assoc dist item (inc (item dist)))))
+
+(defn normalize [dist]
+  "Normalizes the dist for computing the probability of a item from the dist."
+  (let [denom (apply + (vals dist))]
+    (reduce-kv (fn [m k v] (assoc m k (/ v denom))) {} dist)))

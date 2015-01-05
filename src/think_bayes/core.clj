2433a6cf2c632b467bb6e4c5b8b4ae69cb83cb5a
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-05 14:06:15 -0500

    Finished initial bayesian framework from Chapter 2.

--- src/think_bayes/core.clj
+++ src/think_bayes/core.clj
@@ -1,25 +1,43 @@
 (ns think-bayes.core)
 
-(defn distribution []
-  {})
-
-(defn set-prob [dist item pr]
-  (assoc dist item pr))
-
-(defn prob [dist item]
-  (item dist))
-
-(defn increment [dist item]
-  (if (nil? (item dist))
-    (conj dist {item 0})
-    (assoc dist item (inc (item dist)))))
-
-(defn mult [dist item likelihood]
-  (if (nil? (item dist))
-    dist
-    (assoc dist item (* (item dist) likelihood))))
-
 (defn normalize [dist]
-  "Normalizes the dist for computing the probability of a item from the dist."
+  "Normalizes the dist for computing the probability of a hypo from the dist."
   (let [denom (apply + (vals dist))]
     (reduce-kv (fn [m k v] (assoc m k (/ v denom))) {} dist)))
+
+(defn distribution [hypos]
+  "Creates a distribution with given collection of hypos and each having the same probability."
+  (normalize (zipmap (map keyword hypos)
+                (repeat (count hypos) 1))))
+
+(defn set-prob [dist hypo pr]
+  (assoc dist hypo pr))
+
+(defn prob [dist hypo]
+  (hypo dist))
+
+(defn increment [dist hypo]
+  (if (nil? (hypo dist))
+    (conj dist {hypo 1})
+    (assoc dist hypo (inc (hypo dist)))))
+
+(defn mult [dist hypo likelihood]
+  "Multiplies the probability of hypo in dist by the given likelihood. Returns the distribution with updated entries."
+  (if (nil? (hypo dist))
+    dist
+    (assoc dist hypo (* (hypo dist) likelihood))))
+
+(defn likelihood [mixes hypo data]
+  "Computes the likelihood of the data given the hypo and mixes map."
+  (data (hypo mixes)))
+
+(defn update-prob [dist mixes data]
+  "Generates a new distribution based on the mixes and given data."
+  (let [updated-dist (loop [hypos (keys dist)
+                            mod-dist dist]
+                       (if (empty? hypos)
+                         mod-dist
+                         (recur 
+                           (rest hypos)
+                           (mult mod-dist (first hypos) (likelihood mixes (first hypos) data)))))]
+    (normalize updated-dist)))
--- test/think_bayes/core_test.clj
+++ test/think_bayes/core_test.clj
@@ -4,9 +4,16 @@
 
 ; Cookie problem example
 ; Bowl 1 has 30 vanilla, 10 chocolate cookies; Bowl 2 has 20 vanilla, 20 chocolate
-(let [bowl-dist {:bowl1 1 :bowl2 1} ; one of each bowl
-      norm-bowl-dist (normalize bowl-dist)
-      posterior-dist (normalize (mult 
-                                  (mult norm-bowl-dist :bowl1 0.75) ; likelihood of puling vanilla from bowl 1
-                                  :bowl2 0.5))] ; likelihood of pulling vanilla from bowl 2
-  (println "Posterior distribution = " posterior-dist))
+(defn cookie-problem []
+  (let [bowl-dist {:bowl1 1 :bowl2 1} ; one of each bowl
+       norm-bowl-dist (normalize bowl-dist)
+       posterior-dist (normalize (mult 
+                                   (mult norm-bowl-dist :bowl1 0.75) ; likelihood of puling vanilla from bowl 1
+                                   :bowl2 0.5))] ; likelihood of pulling vanilla from bowl 2
+   (println "Posterior distribution = " posterior-dist)))
+
+; Cookie problem example using the bayesian framework
+(defn cookie-problem2 []
+  (let [test-dist (distribution ["bowl1" "bowl2"])
+        mixes {:bowl2 {:choc 0.5, :van 0.5}, :bowl1 {:choc 0.25, :van 0.75}}]
+    (println "Posterior distribution = " (update-prob test-dist mixes :van))))
